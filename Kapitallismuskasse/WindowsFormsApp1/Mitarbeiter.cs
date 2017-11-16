d0dbf90a6abe012c9a024ce92ed2085cfe7d1473
Enzo Aufschnaiter <enzo.aufschnaiter@hak-kitz.ac.at>
2017-11-16 08:42:49 +0100

    Initialisierung

---
+++ Kapitallismuskasse/WindowsFormsApp1/Mitarbeiter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class frm_Mitarbeiter : Form
+    {
+        public frm_Mitarbeiter()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_Mitarbeiter_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
